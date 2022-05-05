ae0b2f9ddab6faaf899cd4f501906efa36184a82
Oscar Voit <oscarvoit@gmail.com>
2022-05-05 20:40:47 -0300

    add tests

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,12 @@
 import express from 'express'
-import nodemailer from 'nodemailer'
 import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-adapter';
-import { prisma } from './prisma';
 import { PrismaFeedbacksRepository } from './repositories/prisma/prisma-feedbacks-repository';
 import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 
 export const routes = express.Router()
 
 routes.post('/feedbacks', async (req, res) => {
+
   const { type, comment, screenshot } = req.body
 
   const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
@@ -18,11 +17,11 @@ routes.post('/feedbacks', async (req, res) => {
     nodemailerMailAdapter
   )
 
-  await SubmitFeedbackUseCase.execute({
+  await submitFeedbackUseCase.execute({
     type,
     comment,
     screenshot,
-  })
+  });
 
   return res.status(201).send();
 })
